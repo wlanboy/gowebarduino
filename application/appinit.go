4dcb7b16e7bb6306a7f38fd6f775d0065ef8483d
wlanboy <wlanboy@github.com>
2026-05-24 09:29:18 +0200

    added module added logging

--- application/appinit.go
+++ application/appinit.go
@@ -1,7 +1,9 @@
 package application
 
 import (
-	arduino "../arduino"
+	"log"
+
+	arduino "github.com/wlanboy/gowebarduino/arduino"
 	"github.com/gorilla/mux"
 )
 
@@ -10,9 +12,13 @@ func (goservice *GoService) Initialize() {
 	goservice.Router = mux.NewRouter()
 
 	goservice.Router.HandleFunc("/api/v1/command", goservice.PostCreate).Methods("POST")
-	goservice.Router.HandleFunc("/api/v1/command", goservice.Get).Methods("Get")
+	goservice.Router.HandleFunc("/api/v1/command", goservice.Get).Methods("GET")
 
-	var console arduino.Arduino = arduino.CreateConsole();
+	console, err := arduino.CreateConsole()
+	if err != nil {
+		log.Printf("Warning: could not open Arduino serial port: %v", err)
+		return
+	}
 	goservice.Console = &console
 }
 
